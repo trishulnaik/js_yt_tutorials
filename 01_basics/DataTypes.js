d7479b67da496b560c512eb00853b55739c9460a
Bannoth Trishul Naik <86940212+trishulnaik@users.noreply.github.com>
2024-03-05 03:48:45 +0000

    DataTypes in JS

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,20 @@
+/* 
+    Types of dataTypes in JS
+    // Primitive types
+    1. Number
+    2. BigInt
+    3. String
+    4. Boolean
+    5. Symbol
+    6. null
+    7. undefined
+    
+    Object Type
+*/
+
+console.table([typeof null, typeof 32, typeof "trishul", typeof undefined]);
+
+/*
+    Typeof null is object,
+    Typeof undefined is undefined
+*/
